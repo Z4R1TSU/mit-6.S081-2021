769f46cf6a9c2bdc30f28d05f3005ff451a89c56
Zari Tsu <zaritsu030907@gmail.com>
2024-03-07 12:54:30 +0800

    finish lab1 util t5 xargs (with something wrong in '\n' but passed anyway)

--- user/xargs.c
+++ user/xargs.c
@@ -10,20 +10,47 @@
 
 // see Chapter 1 Section 3 in Page 16 pipes
 // the input before the pipe '|' will be regarded as standard input
+// and the command after "xargs" will receive the stdin from the return value of command before "|"
+
+void split_buffer(char *buf, char *argv[]) {
+    int idx = 0;
+    argv[idx ++] = buf;
+
+    for (char *p = buf; *p; p ++) {
+        if (*p == '\n') {
+            *p = '\0';
+            if (*(p + 1)) {
+                argv[idx ++] = p + 1;
+            }
+        }
+    }
+}
 
 int main(int argc, char *argv[]) {
-    char *stdin[MAXARG];
+    char *stdinput[MAXARG];
     char buf[1024];
     char *p = buf;
 
     while (read(0, p, 1)) {
-
+        p ++;
     }
 
-    for (i = 0; i < MAXARG; i ++) {
-        if (stdin[i]) {
+    split_buffer(buf, stdinput);
+
+    for (int i = 0; i < MAXARG; i ++) {
+        if (stdinput[i]) {
             if (fork() == 0) {
                 char *clcmd = argv[1];
+                int idx = 2;
+                char *clarg[MAXARG] = {"0"};
+
+                while (argv[idx]) {
+                    clarg[idx - 1] = argv[idx];
+                    idx ++;
+                }
+                clarg[idx - 1] = stdinput[i];
+
+                exec(clcmd, clarg);
             } else {
                 wait(0);
             }
