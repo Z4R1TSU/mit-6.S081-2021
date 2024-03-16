ac52bc61ea7507c102db923cd964cc5a14c3087c
Zari Tsu <zaritsu030907@gmail.com>
2024-03-16 11:58:33 +0800

    update lab9 fs t2 symlink but not pass

--- grade.md
+++ grade.md
@@ -21,3 +21,30 @@
 wrote 65803 blocks
 bigfile done; ok
 
+-----
+
+2. symbolic link
+
+    > main idea:  
+    Symbolic links (or soft links) refer to a linked file by pathname.  
+    `symlink(char *target, char *path)` system call creates a new symbolic link at path that points to file named by target.
+
+    TODO: 
+    
+    * [x]  create a new system call number for `symlink`, add an entry to `user/usys.pl`, `user/user.h`, and implement an empty `sys_symlink` in `kernel/sysfile.c`.
+
+    * [x] Add a new file type (`T_SYMLINK`) to `kernel/stat.h` to represent a symbolic link
+
+    * [x] Add a new flag to `kernel/fcntl.h`, (`O_NOFOLLOW = 0x004`) to open system call. Note that flags passed to open are combined using a bitwise OR operator, so your new flag should not overlap with any existing flags. This will let you compile user/symlinktest.c once you add it to the Makefile.
+
+    * [x] store the target path of a symbolic link in the inode's ***data blocks***.
+
+    * [x] `symlink` should return an integer representing success (0) or failure (-1) similar to link and unlink.
+
+    * [x] Modify the `open` system call where the path refers to a symbolic link.  
+    Case 1: If the file does not exist, open must fail.  
+    Case 2: When a process specifies O_NOFOLLOW in the flags to open, open should open the symlink (and not follow the symbolic link)
+
+    * [x] recursively call `symlink()` when the linked file is also a symbolic link. If revursive depth reach threshold (10) return -1
+
+    * [x] You do not have to handle symbolic links to directories.
--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -290,7 +290,7 @@ sys_open(void)
   int fd, omode;
   struct file *f;
   struct inode *ip;
-  int n;
+  int n, i;
 
   if((n = argstr(0, path, MAXPATH)) < 0 || argint(1, &omode) < 0)
     return -1;
@@ -304,16 +304,37 @@ sys_open(void)
       return -1;
     }
   } else {
-    if((ip = namei(path)) == 0){
-      end_op();
-      return -1;
+    for (i = 0; i <= 10; i ++) {
+      if((ip = namei(path)) == 0){
+        end_op();
+        return -1;
+      }
+      ilock(ip);
+
+      if (ip->type == T_SYMLINK && (omode & O_NOFOLLOW) == 0) {
+        if (readi(ip, 0, (uint64)path, 0, MAXPATH) < 0) {
+          iunlockput(ip);
+          end_op();
+          return -1;
+        }
+
+        iunlockput(ip);
+      } else {
+        break;
+      }
     }
-    ilock(ip);
-    if(ip->type == T_DIR && omode != O_RDONLY){
+
+    if (i > 11) {
       iunlockput(ip);
       end_op();
       return -1;
     }
+  }
+
+  if(ip->type == T_DIR && omode != O_RDONLY){
+    iunlockput(ip);
+    end_op();
+    return -1;
   }
 
   if(ip->type == T_DEVICE && (ip->major < 0 || ip->major >= NDEV)){
@@ -482,5 +503,59 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+
+uint64 sys_symlink(void) {
+  char target[MAXPATH], path[MAXPATH];
+  struct inode *ip, *dp;
+
+  if (argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0) {
+    return -1;
+  }
+
+  begin_op();
+
+  if ((ip = create(path, T_SYMLINK, 0, 0)) == 0) {
+    end_op();
+    return -1;
+  }
+
+  if ((dp = namei(target)) == 0) {
+    end_op();
+    return -1;
+  }
+
+  ilock(ip);
+  ilock(dp);
+
+  // if target or path is directory, return error
+  if (ip->type == T_DIR) {
+    iunlockput(ip);
+    iunlockput(dp);
+    end_op();
+    return -1;
+  }
+
+  if (dp->type == T_DIR) {
+    iunlockput(ip);
+    iunlockput(dp);
+    end_op();
+    return -1;
+  }
+
+  if (writei(ip, 0, (uint64)target, 0, strlen(target)) < 0) {
+    iunlockput(ip);
+    iunlockput(dp);
+    end_op();
+    return -1;
+  }
+
+  iunlockput(ip);
+  iunlockput(dp);
+
+  end_op();
+
   return 0;
 }
